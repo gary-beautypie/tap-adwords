442b41590cdac561f8cf5922ed487b8273d200aa
gary-beautypie <71257526+gary-beautypie@users.noreply.github.com>
2021-09-07 11:23:24 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,17 +12,17 @@ setup(name='tap-adwords',
       install_requires=[
           'singer-python==5.1.5',
           'requests==2.20.0',
-          'googleads==17.0.0',
-#           'pytz==2018.4',
-#           'zeep==3.1.0', # googleads dependency, pinned to 3.1.0 (tested version)
+          'googleads==20.0.0',
+          'pytz==2018.4',
+          'zeep==3.1.0', # googleads dependency, pinned to 3.1.0 (tested version)
       ],
-#       extras_require={
-#           'dev': [
-#               'ipdb==0.11',
-#               'pylint',
-#               'nose'
-#           ]
-#       },
+      extras_require={
+          'dev': [
+              'ipdb==0.11',
+              'pylint',
+              'nose'
+          ]
+      },
       entry_points='''
           [console_scripts]
           tap-adwords=tap_adwords:main
